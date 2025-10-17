57e78afc18247b8125a4eb57eb147a1281367567
Md Ashikur Alom Sarkar <ashikuralom5@gmail.com>
2025-10-17 17:26:52 +0600

    Update Solve quadratic equation using Bhaskara's formula.c

--- Solve quadratic equation using Bhaskara's formula.c
+++ Solve quadratic equation using Bhaskara's formula.c
@@ -1,38 +1,20 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-int main(){
-    //variable diclaretion
-    int a,b,c,discriminant;
+int main() {
+    double a,b,c,r1,r2,discriminant;
+    scanf("%lf %lf %lf", &a, &b, &c);
     
-    //get input 3 numbers
-    printf("\n Enter a value of a :");
-    scanf("%d",&a);
-    printf("\n Enter a value of b :");
-    scanf("%d",&b);
-    printf("\n Enter a value of c :");
-    scanf("%d",&c);
+    discriminant = sqrt((b*b) - (4*a*c));
+    r1 = (-b + discriminant) /(2*a);
+    r2 = (-b - discriminant) /(2*a);
     
-    //calculate discriminant
-    discriminant = (b*b) - (4*a*c);
-    
-    //check real numbers
-    if(discriminant > 0 && a != 0){
-        float root1,root2;
-        
-        //square root of discriminant
-        discriminant = sqrt(discriminant);
-
-        //calculate root1 and root2
-        root1 = (-b + discriminant)/(2*a);  
-        root2 = (-b - discriminant)/(2*a);  
-        
-        printf(" value of root1 is:%f\n",root1);
-        printf(" value of root2 is:%f\n",root2);
+    if(a == 0 || ((b*b) - (4*a*c)) < 0) {
+        printf("Impossivel calcular\n");
     }else{
-        printf(" it is not possible to find the roots.");
+        printf("R1 = %.5lf\n", r1);
+        printf("R2 = %.5lf\n", r2);
     }
-    
     
     return 0;
 }
